fe5515650a29047ac10f03c6ff92ba5f27ca4e7c
Ash <88268317+Ashking92@users.noreply.github.com>
2022-05-25 18:15:10 +0530

    Update u.sh

--- u.sh
+++ u.sh
@@ -4,7 +4,7 @@ git config --global user.email "jetus.hack@yahoo.com"
 git config --global user.name "Jetushack1"
 git commit -m "SOME FILES OF ENCODED"
 git branch -M master
-git remote add origin https://github.com/jetushack1/bot-doge-olduser.git
+git remote add origin https://github.com/Ashking92/tokio-bot-v2.git
 git push -u origin master
-jetus.hack@yahoo.com
+black92devil74@gmail.com 
 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
